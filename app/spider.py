dac61d821a935d431da08b7be3e3c92d21927c90
unknown <rakkakeren@gmail.com>
2022-07-05 11:14:40 +0700

    rename Olshop -> Spider
    add scroll functionality
    add __main__ at the bottom

--- app/spider.py
+++ app/spider.py
@@ -6,28 +6,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-class Olshop:
-
-    def __init__(self):
-        self.head = create_options(headless=True) # kalau false baru bisa
+class Spider:
+    def __init__(self, headless: bool = True):
+        self.head = create_options(headless=headless)
         self.driver = create_driver("chromedriver.exe", options=self.head)
         self.wait = WebDriverWait(self.driver, 5)
 
+    def scroll(self):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-class Tokopedia(Olshop):
+
+class Tokopedia(Spider):
     
     def __init__(self, url):
         super().__init__()
         self.url = self.driver.get(url)
 
     def search(self, keyword):
-    # Wait then find search bar
         try: 
+            # Wait then find search bar
             search_bar = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".e110g5pc0")))
             search_bar.clear() # search bar ga mau diclear
             search_bar.send_keys(keyword)
 
-    #find submit button
+             # find submit button
             search_button = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".css-1czin5k")))
             search_button.click()
         except:
@@ -41,16 +43,16 @@ class Tokopedia(Olshop):
         return self.driver.quit()
 
 
-class Shopee(Olshop):
+class Shopee(Spider):
     pass
 
 
-tokopedia = Tokopedia(url = "https://tokopedia.com")
-
-tokopedia
-tokopedia.search("iphone 13")
-tokopedia.snapshot()
-tokopedia.quit()
+if __name__ == "__main__":
+    tokopedia = Tokopedia(url="https://tokopedia.com", headless=False)
+    # tokopedia.search("iphone 13")
+    tokopedia.snapshot()
+    tokopedia.scroll()
+    tokopedia.quit()
 
 
 
