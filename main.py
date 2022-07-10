97d26e3047da1387e7de562cd7f1f59cc8322645
unknown <rakkakeren@gmail.com>
2022-07-11 00:06:40 +0700

    add argparse and if main test

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import argparse
 
+from app.driver import DefaultWebDriver
+
 parser = argparse.ArgumentParser(description="Online Shop scrapper with selenium")
-parser.add_argument("--no-headless", action="store_true", help="run webdriver without headless mode")
+
+parser.add_argument(
+    "--headless", action="store_true", default=True, help="run webdriver with headless mode (default)")
+parser.add_argument(
+    "--no_headless", action="store_false", dest="headless", help="run webdriver without headless mode")
+
 
 args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    driver = DefaultWebDriver(headless=args.headless).create_driver()
+    driver.get("https://google.com")
+    driver.save_screenshot("google.png")
