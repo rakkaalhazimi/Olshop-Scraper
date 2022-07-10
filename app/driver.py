0901de18b9690a4cfc800a1a25fe6d49bcb41e76
unknown <rakkakeren@gmail.com>
2022-07-10 18:18:20 +0700

    setup new webdriver class

--- app/driver.py
+++ app/driver.py
@@ -7,6 +7,40 @@ OS_NAME = platform.system()
 LINUX_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
 WIN_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
 
+
+class WebDriver:
+    def __init__(self, headless: bool):
+        options = webdriver.ChromeOptions()
+
+        # Headless mode
+        if headless:
+            options.add_argument("--headless")
+
+        # User agent
+        if OS_NAME == "Windows":
+            options.add_argument(f"user-agent={WIN_USER_AGENT}")
+        else:
+            options.add_argument(f"user-agent={LINUX_USER_AGENT}")
+
+        # Default options
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--allow-running-insecure-content")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--proxy-server='direct://'")
+        options.add_argument("--proxy-bypass-list=*")
+        options.add_argument("--start-maximized")
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+
+        # Instantiate webdriver
+        self._driver = webdriver.Chrome(options=options)
+
+    def create_driver(self):
+        return self._driver
+
+
 def create_options(
         headless: bool = True,
         resolution: Tuple[int, int] = (1920, 1080),
@@ -47,8 +81,5 @@ def create_driver(
 
 
 if __name__ == "__main__":
-    options = create_options(headless=False, resolution=(1366, 768))
-    driver = create_driver(options=options)
-    driver.get("https://google.com")
-    driver.save_screenshot("google.png")
+    driver = WebDriver(headless=True).create_driver()
     driver.quit()
