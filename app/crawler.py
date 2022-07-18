789e462050ebc28314aff065804214c46000da63
unknown <rakkakeren@gmail.com>
2022-07-19 00:40:27 +0700

    print every step in crawler

--- app/crawler.py
+++ app/crawler.py
@@ -30,22 +30,28 @@ class MultiPageCrawler(Crawler):
     def execute(self):
 
         self.spider.start()
+        print(f"[RUN] Scraping {self.spider.name}")
 
         if self.search_bar:
+            print("[RUN] Using search bar")
             self.spider.search(self.search_keyword, self.search_bar)
 
         try:
             for page in range(2, self.pages + 2):
                 self.spider.driver.implicitly_wait(1)
+                print("[RUN] scrolling...")
                 self.spider.scroll_until_bottom()
 
                 if self.contents_parent and self.contents:
+                    print("[RUN] scraping website..")
                     records = self.spider.grab_by_parent(
                         parent=self.contents_parent,
                         contents=self.contents
                     )
+                    print("[RUN] inserted into mongo cluster")
                     self.spider.to_mongo_cluster(records)
 
+                print("[RUN] go to the next page")
                 self.spider.redirect_next_page(page=page)
 
         finally:
