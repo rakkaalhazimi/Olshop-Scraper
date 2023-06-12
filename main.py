116f6f25fa251d019d126ac92f3ef913ca4d8c19
unknown <rakkakeren@gmail.com>
2023-06-13 05:43:21 +0700

    feat: save products to jsonl

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import json
+
 from olshop_scrapper.scrapper import TokopediaScrapper
 
 
 class Application:
     def start(self):
-        scrapper = TokopediaScrapper(headless=False)
-        scrapper.find_product("laptop asus")
+        scrapper = TokopediaScrapper(headless=True)
+        products = scrapper.find_product("laptop asus", pages=2)
+
+        with open("result.jsonl", "w") as file:
+            for product in products:
+                file.write(json.dumps(product))
+                file.write("\n")
+
         scrapper.driver.quit()
 
 
